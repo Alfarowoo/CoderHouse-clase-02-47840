9ed19aab762a98011818db9aac834709240a73f8
Fabian Alfaro <fabianalfaro10@gmail.com>
2023-06-18 19:11:36 -0300

    CoderHouse-clase-02-47840

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { MainWrapperComponent } from './main-wrapper/main-wrapper.component';
+import { MyPComponent } from './components/my-p/my-p.component';
 
 @NgModule({
   declarations: [
-    AppComponent
-  ],
+     AppComponent,
+     HeaderComponent,
+     SidebarComponent,
+     MainWrapperComponent,
+     MyPComponent,
+  // AnotherComponet1,
+  // AnotherComponet2,
+  // AnotherComponet3,
+  ], // AppComponent pertenece a AppModule
   imports: [
     BrowserModule,
     AppRoutingModule
